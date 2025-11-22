09c3ab0edaef0bc01d2fd917420fedc1215b7274
Vachhani Vandani <vachhanivandani@gmail.com>
2025-11-22 20:41:18 +0530

    prepare all for deployment ready

--- server.js
+++ server.js
@@ -108,19 +108,19 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB)
-  .then(() => console.log('DB connection successful!'))
-  .catch(err => console.log('DB connection error:', err));
+  // .then(() => console.log('DB connection successful!'))
+  // .catch(err => console.log('DB connection error:', err));
 
 
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  // console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shu/tting down...');
+  // console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
